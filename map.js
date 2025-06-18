8a5209c6840944fe82c65a00382a61a402ddc1b7
imran <otcimran16@gmail.com>
2025-06-18 09:40:23 +0300

    commits

---
+++ map.js
@@ -0,0 +1,16 @@
+const tutorial  = [
+  'What Does The This Keyword Mean?',
+  'What Is The Constructor OO Pattern?',
+  'Implementing Blockchain Web API',
+  'The Test Driven Development Workflow',
+  'What Is NaN And How Can We Check For It',
+  'What Is The Difference Between StopPropagation And PreventDefault?',
+  'Immutable State And Pure Functions',
+  'What Is The Difference Between == And ===?',
+  'What Is The Difference Between Event Capturing And Bubbling?',
+  'What Is JSONP?'
+];
+
+const titleCased = () => {
+  return tutorial
+}
